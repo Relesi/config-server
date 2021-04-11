57e8a1c95e3e63e269b64284c7817087d9f28b75
relesi <renatolessa.2011@hotmail.com>
2021-04-11 16:32:17 -0300

    implement config serve to service

---
+++ src/test/java/br/com/relesi/cloud/service/config/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.relesi.cloud.service.config;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
