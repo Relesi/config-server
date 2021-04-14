8398450d1b56001d7a74b59329fcd41105668072
relesi <renatolessa.2011@hotmail.com>
2021-04-13 21:07:13 -0300

    ajust confi yml

--- src/test/java/br/com/relesi/cloud/service/config/ConfigServerApplicationTests.java
+++ src/test/java/br/com/relesi/cloud/service/config/ConfigServerApplicationTests.java
@@ -1,13 +1,13 @@
-package br.com.relesi.cloud.service.config;
-
-import org.junit.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class ConfigServerApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package br.com.relesi.cloud.service.config;
+//
+//import org.junit.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class ConfigServerApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
